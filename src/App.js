dd298316e993f4147763441f946ea853a61fdd38
alfred-moyo <alf.moxz@gmail.com>
2025-03-11 23:02:27 +0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import 'animate.css'
 
 function App() {
   return (
     <div className="App">
-      <nav>
-        <img src={""} className="app-logo" alt="logo" />
-        <h2>Write a Review</h2>
-
-      </nav>
+      <body>
+        <nav>
+          <img src={logo} className="App-logo" alt="logo" />
+          <ul>
+            <li><a href="#about">Write a review</a></li>
+            <li><a href="business"> Business</a></li>
+          </ul>
+          <button type="button">LOGIN</button>
+        </nav>
+        <div class="container">
+          <h1>ZilBiz</h1>
+          <h3>Empower Your Business, Elevate Your Presence! <span>🚀</span></h3>
+          <p>
+          Welcome to ZilBiz, Mauritius’s premier SME listing and review platform! 
+          Whether you're a small business owner looking to grow or a customer searching 
+          for the best local services, we’ve got you covered. ✨
+          </p>
+          <button>Register Your Business</button>
+        </div>
+        <div class="spotlight">
+          {/* <h2>Spotlight</h2> */}
+        </div>
+          
+      </body>
       
     </div>
   );
